86d32a74699c03b389af3a3001907526f642ee29
mdervaux <maxime.dervaux@ittalent.fr>
2024-02-06 10:12:07 +0100

    Borrow_Entity_add

---
+++ Models/BorrowEntities/Borrow.entity.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once("../BookEntities/Book.entity.php");
+require_once("../MemberEntities/Member.entity.php");
+Class Borrow {
+
+    public Date $date;
+    public Member $member;
+    public Book $book;
+
+
+
+}
