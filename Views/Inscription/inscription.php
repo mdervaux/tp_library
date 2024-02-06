b4fa9a43747a0a3881bd029cfce95b644ddd3f73
Julien FORESTIER <julien.forestier@axa.fr>
2024-02-06 14:43:04 +0100

    fix: style page inscription

--- Views/Inscription/inscription.php
+++ Views/Inscription/inscription.php
@@ -1,16 +1,16 @@
 <form class="w-full max-w-sm"  action="Controllers/ScriptInscription.php"method="POST">
-  <div class="md:flex md:items-center">
+  <div>
     <div class="md:w-1/3">
-      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+      <label class="block text-gray-500 font-bold  mb-1 md:mb-0 pr-4" for="inline-full-name">
         identifiant
       </label>
     </div>
     <div class="md:w-2/3">
       <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" name="npm_utilisateur">
     </div>
-    <div class="md:flex md:items-center mb-6">
+    <div class="mb-6">
     <div class="md:w-1/3">
-      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+      <label class="block text-gray-500 font-bold  mb-1 md:mb-0 pr-4" for="inline-full-name">
         email
       </label>
     </div>
@@ -18,9 +18,9 @@
       <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" name="email">
     </div>
   </div>
-  <div class="md:flex md:items-center mb-6">
+  <div class=" mb-6">
     <div class="md:w-1/3">
-      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
+      <label class="block text-gray-500 font-bold  mb-1 md:mb-0 pr-4" for="inline-password">
         mot de passe
       </label>
     </div>
@@ -32,7 +32,7 @@
     <div class="md:w-1/3"></div>
     
   </div>
-  <div class="md:flex md:items-center">
+  <div>
     <div class="md:w-1/3"></div>
     <div class="md:w-2/3">
       <button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
