47551153d6ba855f85a7e292607517a6fb75b693
Julien FORESTIER <julien.forestier@axa.fr>
2024-02-06 10:28:32 +0100

    fix: member factory

--- Services/MemberFactory.php
+++ Services/MemberFactory.php
@@ -1,8 +1,8 @@
 <?php
 class MemberFactory {
-    public static function createMember($member) {
+    public static function createMember($role) {
         switch ($role) {
-            case $member->admin:
+            case Role::ADMIN:
                 return new Admin();
             case $member->member:
                 return new Member();
@@ -10,16 +10,4 @@ class MemberFactory {
                 throw new Exception("Invalid role: $member->role");
         }
     }
-}
-
-class Admin {
-    // Admin class implementation
-}
-
-class User {
-    // User class implementation
-}
-
-class Guest {
-    // Guest class implementation
 }
