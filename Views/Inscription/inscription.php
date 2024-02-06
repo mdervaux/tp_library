218eb044a8da1424c723964f6b4591648c1fbb76
Julien FORESTIER <julien.forestier@axa.fr>
2024-02-06 14:31:38 +0100

    feature: script inscription

--- Views/Inscription/inscription.php
+++ Views/Inscription/inscription.php
@@ -1,12 +1,21 @@
-<form class="w-full max-w-sm"  action="scriptInscription.php"method="POST">
-  <div class="md:flex md:items-center mb-6">
+<form class="w-full max-w-sm"  action="Controllers/ScriptInscription.php"method="POST">
+  <div class="md:flex md:items-center">
     <div class="md:w-1/3">
       <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
         identifiant
       </label>
     </div>
     <div class="md:w-2/3">
-      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" name="identifiant">
+      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" name="npm_utilisateur">
+    </div>
+    <div class="md:flex md:items-center mb-6">
+    <div class="md:w-1/3">
+      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+        email
+      </label>
+    </div>
+    <div class="md:w-2/3">
+      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" name="email">
     </div>
   </div>
   <div class="md:flex md:items-center mb-6">
@@ -16,7 +25,7 @@
       </label>
     </div>
     <div class="md:w-2/3">
-      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" name="password">
+      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" name="mot_de_passe">
     </div>
   </div>
   <div class="md:flex md:items-center mb-6">
