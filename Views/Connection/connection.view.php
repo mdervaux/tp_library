575ccf85f1191730bfc985e5ab620a51952f4253
NathanFaucon <nathan.faucon@viacesi.fr>
2024-02-06 16:18:25 +0100

    change_username_input

--- Views/Connection/connection.view.php
+++ Views/Connection/connection.view.php
@@ -8,8 +8,8 @@
               </h1>
               <form class="space-y-4 md:space-y-6" action="Controllers/ScriptConnection.php" method="POST">
                   <div>
-                      <label  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Votre email :</label>
-                      <input type="text" name="name"  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="votre@email.com" required="">
+                      <label  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Votre nom d'utilisateur :</label>
+                      <input type="text" name="name"  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Username" required="">
                   </div>
                   <div>
                       <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mot de passe :</label>
