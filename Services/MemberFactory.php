4d5a631eb342d732c16d82849e70383c00cbe72d
Julien FORESTIER <julien.forestier@axa.fr>
2024-02-06 10:38:35 +0100

    feature: member factory

--- Services/MemberFactory.php
+++ Services/MemberFactory.php
@@ -1,11 +1,11 @@
 <?php
 class MemberFactory {
-    public static function createMember($role) {
+    public static function createMember($role, $name, $mail, $password) {
         switch ($role) {
             case Role::ADMIN:
-                return new Admin();
+                return new Admin($name, $mail, $password);
             case Role::MEMBER:
-                return new Member();
+                return new Member($name, $mail, $password);
             default:
                 throw new Exception("Invalid role: $member->role");
         }
